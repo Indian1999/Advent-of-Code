d07ddec603b5a327c7373cf4ab1b3afac17f8181
Indian1999 <haszab99@gmail.com>
2024-12-11 04:00:35 +0100

    day 1 part 2 done

--- day 1/main.py
+++ day 1/main.py
@@ -6,11 +6,23 @@ with open("day 1/input.txt", "r", encoding="utf-8") as f:
         list_1.append(int(line.split()[0]))
         list_2.append(int(line.split()[1]))
         
-list_1.sort()
-list_2.sort()
+#list_1 = [3,4,2,1,3,3]
+#list_2 = [4,3,5,3,9,3]
+def part1():
+    list_1.sort()
+    list_2.sort()
 
-total = 0
-for i in range(len(list_1)):
-    total += abs(list_1[i]-list_2[i])
-    
-print("Total difference:", total)
+    total = 0
+    for i in range(len(list_1)):
+        total += abs(list_1[i]-list_2[i])
+
+    print("Total difference:", total)
+
+def part2():
+    total = 0
+    for item in list_1:
+        total += item * list_2.count(item)
+    print(total)
+
+#part1()
+part2()
