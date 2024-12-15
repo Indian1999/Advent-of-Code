01819c8b94793e68dee7e6bbc3a56b4dd541ab49
Indian1999 <haszab99@gmail.com>
2024-12-15 02:15:32 +0100

    day 14 part 1 done

--- day 14/main.py
+++ day 14/main.py
@@ -1,4 +1,4 @@
-
+from_file = True
 robots = [
     ((0,4), (3,-3)),
     ((6,3), (-1,-3)),
@@ -13,26 +13,30 @@ robots = [
     ((2,4), (2,-3)),
     ((9,5), (-3,-3))
 ]
-robots = []
-with open("day 14/input.txt", "r", encoding="utf-8") as f:
-    # Sample line: p=35,60 v=-8,52
-    for line in f:
-        line_split = line.split(" v=")
-        pos_str = line_split[0][2:]
-        vel_str = line_split[1]
-        pos_str_split = pos_str.split(",")
-        vel_str_split = vel_str.split(",")
-        robots.append(
-            (
-                (int(pos_str_split[0]), int(pos_str_split[1])),
-                (int(vel_str_split[0]), int(vel_str_split[1]))
-            )
-        )
-
 width = 11
 height = 7
+if from_file:
+    width = 101
+    height = 103
+    robots = []
+    with open("day 14/input.txt", "r", encoding="utf-8") as f:
+        # Sample line: p=35,60 v=-8,52
+        for line in f:
+            line_split = line.split(" v=")
+            pos_str = line_split[0][2:]
+            vel_str = line_split[1]
+            pos_str_split = pos_str.split(",")
+            vel_str_split = vel_str.split(",")
+            robots.append(
+                (
+                    (int(pos_str_split[0]), int(pos_str_split[1])),
+                    (int(vel_str_split[0]), int(vel_str_split[1]))
+                )
+            )
+
 
 def calculate_positions(robots, seconds):
+    # Sample robot  (pos, vel) = ((0,4), (3,-3))
     new_positions = []
     for robot in robots:
         new_pos = (robot[0][0] + robot[1][0] * seconds, robot[0][1] + robot[1][1] * seconds)
@@ -43,13 +47,13 @@ def calculate_positions(robots, seconds):
 def count_robots(robots):
     q1, q2, q3, q4 = 0, 0, 0, 0
     for robot in robots:
-        if robot[0] < width // 2 - 1 and robot[1] < height // 2 - 1:
+        if robot[0] < width // 2 and robot[1] < height // 2:
             q1 += 1
-        if robot[0] > width // 2 - 1 and robot[1] < height // 2 - 1:
+        if robot[0] > width // 2 and robot[1] < height // 2:
             q2 += 1
-        if robot[0] > width // 2 - 1 and robot[1] > height // 2 - 1:
+        if robot[0] > width // 2 and robot[1] > height // 2:
             q3 += 1
-        if robot[0] < width // 2 - 1 and robot[1] > height // 2 - 1:
+        if robot[0] < width // 2 and robot[1] > height // 2:
             q4 += 1
     print(q1,q2,q3,q4)
     return q1*q2*q3*q4
@@ -59,11 +63,15 @@ def robot_matrix(robots):
     for pos in robots:
         robot_matrix[pos[1]][pos[0]] += 1
     for row in robot_matrix:
+        row[width//2] = " "
+    robot_matrix[height//2] = [" " for i in range(int(width*0.7))]
+    for row in robot_matrix:
         print(row)
-
+    
 new_positions = calculate_positions(robots, 100)
 #robot_matrix(new_positions)
-print(new_positions)
 print(count_robots(new_positions))
 
 #part 1: 63011520 INCORRECT
+#part 1: 221604840 INCORRECT (too low)
+#part 1: 225810288 CORRECT
