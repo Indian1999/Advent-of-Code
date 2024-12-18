9898abe34b0c7e62eba80a2d7a6247f093160ad4
Indian1999 <haszab99@gmail.com>
2024-12-19 00:11:47 +0100

    day 17 part 2 seems impossible

--- day 17/main.py
+++ day 17/main.py
@@ -103,13 +103,58 @@ def loop():
 
 #From doodling on paper I deduced that the goal number is 48 bit long and the first 4 bits are 1110
 # First bits: 1110000
+# At this point the algorithm got stuck in a loop
+# a = 60507697584 (111000010110100010100011000110110000): [7, 5, 0, 3, 4, 3, 1, 6, 5, 5, 3, 0]
+"""
+solution_triads = ["111", "000"]
+solution = solution_triads[0] + solution_triads[1]
+while len(solution_triads) < len(instructions):
+    triads = ["000", "001", "010", "011", "100", "101", "110", "111"]
+    for item in triads: 
+        num = solution + item
+        a = int(num,2)
+        n = len(solution_triads) + 1
+        loop()
+        print(f"a = {int(num,2)} ({num}):", output)
+        if len(output) >= n and output[-n:] == instructions[-n:]:
+            #print(f"a = {int(num,2)} ({num}):", output)
+            solution_triads.append(item)
+            solution += item
+            break
+    else:
+        solution_triads.append("000")
+        solution += "000"
+"""
+"""
+solution = "111000010110100010100011000110110" #15 more bits needed
+#solution = "111000010110100010110011000110110" #15 more bits needed
+triads = ["000", "001", "010", "011", "100", "101", "110", "111"]
+for triad0 in triads:
+    for triad1 in triads:
+        for triad2 in triads:
+            for triad3 in triads:
+                for triad4 in triads:
+                    for triad5 in triads:
+                        num = solution + triad0 + triad1 + triad2 + triad3 + triad4 + triad5
+                        a = int(num,2)
+                        loop()
+                        #print(output)
+                        if output[-16:] == instructions[-16:]:
+                            print(f"{triad0}, {triad1}, {triad2}, {triad3}, {triad4}, {triad5}: {output[:5]}")
 
-
-for i in range(200000, 800000000):
+print(solution)
+print(int(solution,2))
+"""
+start = "111000010110100010100011000110110" + "0"*15
+start = int(start,2)
+for i in range(start, start + 2**16):
     a = i
     loop()
-    if len(output) >= 5 and output[-5:] == instructions[-5:]:
-        print(f"a = {i} ({bin(i)[2:]}):", output)
+    if output[5] == instructions[5] and output[4] == instructions[4]:
+        print(output)
+    if output == instructions:
+        print(i)
+
 
 """
 Nice try, the value of our searched value looks to be a 46-48 bit number
@@ -123,5 +168,25 @@ while output != instructions:
 print(output)
 """
 
+def int_to_bin(num):
+    bin_str = bin(num)[2:]
+    out = ""
+    i = len(bin_str) - 1
+    while i >= 0:
+        out += bin_str[i]
+        if (len(bin_str) - 1 - i) % 3 == 2:
+            out += "."
+        i -= 1
+    return out
+"""
+n = 7
+for i in range(2**(n*3), 2**(n*3 + 3)):
+    a = i
+    loop()
+    if (output[-n:] == instructions[-n:]):
+        print(int_to_bin(i), output)
+"""
+
+#Goal: 2,4,1,1,7,5,0,3,4,3,1,6,5,5,3,0
 
 #part 1: 2,0,7,3,0,3,1,3,7 CORRECT
